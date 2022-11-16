fcb5dafbb97dcab23cc0d95a85b8423cf9d82fd8
Hemant Khinchi <70513827+hemant198@users.noreply.github.com>
2022-11-16 15:48:02 +0000

    showing loop example

--- script.js
+++ script.js
@@ -1,103 +1,32 @@
-// */camel Notation: oneTwoThreeFour */ for factor function
-// */pascal Notation: OneTwoThreeFour */ for constructor function
-const radius = {
-  radius: 1,
-  location: {
-    x: 2,
-    y: 3,
-  },
-  draw: function draw() {
-    console.log('Draw circle');
-  }
+//for loop
+for (i = 0; i <= 10; i++) {
+  console.log("in loop", i)
+  document.getElementById("demo").innerHTML += `<br/>${i} `
 }
-radius.draw()
-// factory function example used for stop multiple time repeat of the object.
-function createCircle(radius) {
-  return radius = {
-    //radius: radius, #in the modern javascript key and value are same then we can only pass the key that is equlient.
-    radius,
-    draw() {
-      console.log('Draw circle new');
-    }
-  }
-}
-
-const circle_1 = createCircle(1);
-console.log(circle_1);
-const circle_2 = createCircle(2);
-console.log(circle_2);
-
-
-// #Constructor function /*the constructor function job is to construct or create an new object*/
-function Circle(radius) {
-  this.radius = radius;
-  this.draw = function() {
-    console.log('constructor Draw circle');
-  }
-}
-const another = new Circle(25);
-console.log(another.draw);
-//# call method
-Circle.call({}, 1)
-//# apply method
-Circle.apply({}, [1])
-//# bind method
-const box = {
-  name: 'boxObject',
-  ages: 1,
-}
-box.color = "Yellow";//add new property of object
-box.size = "2x";//add new property of object
-box.draw = function() { console.log('Box draw xxxx') }
-//delete
-delete box.size;
-console.log(box.draw())
-
-// constructor property
-
-for (let key in box) {
-  console.log(key, box[key])
-}
-console.log('object key')
-for (let key of Object.keys(box))
-  console.log(key)
-
-for (let entry of Object.entries(box))
-  console.log(entry)
-if ('color' in box) console.log('say: yes')
-//# copy an object in to another object
-const circleNew = {
-  radius: 1,
-  draw() {
-    console.log('draw')
-  }
-}
-// const anotherOne = {};
-// for (let key in circleNew)
-//   anotherOne[key] = circleNew[key];
-console.log('line-change')
-//const anotherOne = Object.assign = ({}, circleNew) /*Morden way*/
-/*another simple way*/
-const anotherOne = { ...circleNew, coloe: 'yellow', }
-console.log(anotherOne)
-
-class Episode {
-  constructor(title, duration, hasBeenWatched) {
-    this.title = title;
-    this.duration = duration;
-    this.hasBeenWatched = hasBeenWatched;
-  }
-}
-let firstEpisode = new Episode('Dark Side', 45, true);
-let secondEpisode = new Episode('The Mystery', 15, false);
-let thirdEpisode = new Episode('The Unexpected Climax', 25, true);
+console.log("loop finiesh")
 //
-document.querySelector('#firstEpisodeInfo').innerText = `Episode: ${firstEpisode.title}
-Duration: ${firstEpisode.duration}
-Already watxhed: ${firstEpisode.hasBeenWatched}: `;
-document.querySelector('#secondEpisodeInfo').innerText = `Episode: ${firstEpisode.title}
-Duration: ${firstEpisode.duration}
-Already watxhed: ${firstEpisode.hasBeenWatched}: `;
-document.querySelector('#thirdEpisodeInfo').innerText = `Episode: ${firstEpisode.title}
-Duration: ${firstEpisode.duration}
-Already watxhed: ${firstEpisode.hasBeenWatched}: `;
+const names = ['shaun', 'mario', 'lugii'];
+for (i = 0; i < names.length; i++) {
+  //console.log(names[i], )
+  let html = `<div>${names[i]}</div>`;
+  console.log(html);
+  document.getElementById("demo").innerHTML += html
+}
+console.log("name loop finiesh");
+
+let result = 'x';
+for (i = 0; i <= 100; i++) {
+  for (k = 0; k <= 10; k++) {
+    if (i == 0 && k > 10) {
+      result += '[' + k + ']';
+    }
+    else if (k == 0 && i > 0) {
+      result += '[' + i + '] ';
+    }// for the number present
+    else if (i > 0 && k > 0) {
+      result += (i * k) + ' ';
+    }// for multiplay
+  }
+  result += '\n'
+}
+console.log(result);
